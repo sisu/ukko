3dcef62f44c63f080d0df0689728dab1d834a495
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2011-05-19 21:22:50 +0300

    some serialization stuff and added .gitignore

--- serialize.hpp
+++ serialize.hpp
@@ -5,6 +5,7 @@
 namespace ukko {
 
 struct Serializer {
+	std::vector<char> buf;
 };
 struct Deserializer {
 	Deserializer(char* buf): buf(buf) {}
@@ -21,28 +22,41 @@ void serialize(S& s, T& t) {
 	t.serialize(s);
 }
 
+#define FORALL_PRIMITIVES(F)\
+F(int8_t); F(int16_t); F(int32_t); F(int64_t);\
+F(uint8_t); F(uint16_t); F(uint32_t); F(uint64_t);\
+F(float); F(double); F(long double);
+
+#define S(type)\
+	void serialize(Serializer& s, type& t) {\
+		char* ct = (char*)&t;\
+		s.buf.insert(s.buf.begin(), ct, ct+sizeof(type));\
+	}
+FORALL_PRIMITIVES(S)
+#undef S
+
 #define S(type)\
 	void serialize(Deserializer& s, type& t) {\
 		t = *(type*)s.buf;\
 	}
-S(int8_t);
-S(int16_t);
-S(int32_t);
-S(int64_t);
-S(uint8_t);
-S(uint16_t);
-S(uint32_t);
-S(uint64_t);
+FORALL_PRIMITIVES(S)
 #undef S
 
+#undef FORALL_PRIMITIVES
+
+template<class T>
+void serialize(Serializer& s, std::vector<T>& v) {
+	s & v.size();
+	for(size_t i=0; i<v.size(); ++i)
+		s & v[i];
+}
 template<class T>
 void serialize(Deserializer& s, std::vector<T>& v) {
 	size_t size;
 	s & size;
 	v.resize(size);
-	for(size_t i=0; i<size; ++i) {
+	for(size_t i=0; i<size; ++i)
 		s & v[i];
-	}
 }
 
 }
